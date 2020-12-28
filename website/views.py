ae6deb3571eb93d1194eb739a2614ac0b846ad33
Abhinav Padala <abhinavpadala@live.com>
2020-12-28 09:45:19 +0100

    feat: Add Meetings

---
+++ website/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from datetime import datetime
+
+def welcome(request):
+    return HttpResponse("Welcome to the Meeting Planner!")
+
+def date(request):
+	return HttpResponse("This page was served at " + str(datetime.now()))
+
+def about(request):
+	 return HttpResponse("Welcome to About Page")
